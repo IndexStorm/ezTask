c13c3be7107db82b7c256b0eb7d06eb255c6c54c
Mike <ovyan13@ya.ru>
2020-07-18 00:26:20 +0300

    added view

--- ezTask/ezTask/ViewController.swift
+++ ezTask/ezTask/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let topView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.231372549, green: 0.4156862745, blue: 0.9960784314, alpha: 1)
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
-        print("started")
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        self.view.addSubview(topView)
+        
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.topAnchor.constraint(equalTo: topView.superview!.topAnchor).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        topView.leadingAnchor.constraint(equalTo: topView.superview!.leadingAnchor, constant: 0).isActive = true
+        topView.trailingAnchor.constraint(equalTo: topView.superview!.trailingAnchor, constant: 0).isActive = true
+
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+          return .lightContent
     }
 
 
